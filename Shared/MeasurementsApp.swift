a3ae3b81e277d9e005bca2a990a8fc3e6e226748
Dan Beers <spanyell@icloud.com>
2020-09-16 10:34:40 -0700

    Initial Commit

---
+++ Shared/MeasurementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasurementsApp.swift
+//  Shared
+//
+//  Created by Dan Beers on 9/16/20.
+//
+
+import SwiftUI
+
+@main
+struct MeasurementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
